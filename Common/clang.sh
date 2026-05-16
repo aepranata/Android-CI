150748917c4dd2cfb4131110d5bb1c119f8575e7
Aditya Eka Pranata <aepranata@gmail.com>
2026-05-16 17:38:35 +0700

    Enable polly project
    Change-Id: I13b8f4628d266f5273502405906e10deeaac299f

--- Common/clang.sh
+++ Common/clang.sh
@@ -35,7 +35,7 @@ msg "Building LLVM's ..."
 chmod +x build-llvm.py
 ./build-llvm.py \
     --defines LLVM_PARALLEL_COMPILE_JOBS="$(nproc)" LLVM_PARALLEL_LINK_JOBS="$(nproc)" CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3  \
-    --projects clang lld compiler-rt libcxx libcxxabi \
+    --projects clang lld compiler-rt libcxx libcxxabi polly \
     --full-toolchain \
     --install-folder "$install" \
     --quiet-cmake \
