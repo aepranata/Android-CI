bc8cc52023a78c250c13d32c62740148f4a9d13d
Aditya Eka Pranata <aepranata@gmail.com>
2026-05-16 17:39:37 +0700

    Remove full-toolchain arg
    This partially revert commit e1167ce2df5807b1a0ee20326d21b2793f4e3984.
    
    Change-Id: If8f3d7ace3b8f27d8dcbb073868c7e1fae0e9ab4

--- Common/clang.sh
+++ Common/clang.sh
@@ -36,7 +36,6 @@ chmod +x build-llvm.py
 ./build-llvm.py \
     --defines LLVM_PARALLEL_COMPILE_JOBS="$(nproc)" LLVM_PARALLEL_LINK_JOBS="$(nproc)" CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3  \
     --projects clang lld compiler-rt libcxx libcxxabi polly \
-    --full-toolchain \
     --install-folder "$install" \
     --quiet-cmake \
     --shallow-clone \
